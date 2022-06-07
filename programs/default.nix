ee726fb3f65728871248ec1ba37e14465136e39e
zzzsyyy <me@zzzsy.top>
2022-06-07 13:45:18 +0800

    waybar into hm

--- programs/default.nix
+++ programs/default.nix
@@ -8,4 +8,12 @@
     ./kitty.nix
     ./git.nix
   ];
+  programs = {
+    waybar = {
+      enable = true;
+      settings = [ (import ./waybar.nix) ];
+      style = builtins.readFile ./waybar.css;
+      systemd.enable = true;
+    };
+  };
 }
---
+++ programs/waybar.nix
@@ -0,0 +1,140 @@
+[
+  {
+    "layer": "top",
+    "position": "top",
+    "height": "22pt",
+    "spacing": 6,
+    "margin": 0,
+    "modules-left": [
+      "sway/workspaces",
+      "sway/mode",
+    ],
+    "modules-center": ["sway/window"],
+    "modules-right": [
+      "memory",
+      "cpu",
+      "battery",
+      "clock",
+      "keyboard-state",
+      "idle_inhibitor",
+      "custom/poweroff"
+    ],
+
+    "sway/workspaces": {
+      "all-outputs": false,
+      "disable-scroll": false,
+      "format": "{name}"
+    },
+    "sway/mode": {
+      "format": "<span style=\"italic\"> {}</span>", // Icon: expand-arrows-alt
+      "tooltip": false
+    },
+    "sway/window": {
+      "format": "{}",
+      "max-length": 40,
+      "rewrite": {
+        "(.*) - Mozilla Firefox": "🌎 $1",
+        "(.*) - vim": " $1",
+        "(.*) - zsh": " [$1]"
+      }
+    },
+    "keyboard-state": {
+      "capslock": true,
+      "format": "{icon}",
+      "format-icons": {
+        "locked": "Cap ",
+        "unlocked": ""
+      }
+    },
+    "idle_inhibitor": {
+      "format": "{icon}",
+      "format-icons": {
+        "activated": "",
+        "deactivated": ""
+      }
+    },
+    "battery": {
+      "bat": "BAT0",
+      "adapter": "AC0",
+      "format": "{icon} {capacity}%",
+      "format-charging": " {capacity}%",
+      "format-full": "{icon} 已充满",
+      "format-icons": ["", "", "", "", "", "", "", ""],
+      "full-at": 96,
+      "format-discharging": "{icon} {capacity}%",
+      "format-alt": "{icon} {time}",
+      "format-time": "{H}:{M:02d}"
+    },
+    "custom/poweroff": {
+      "format": "",
+      "on-click": "exec sh -c ~/.config/waybar/scripts/poweroff.sh",
+      "tooltip": false
+    },
+    "cpu": {
+      "format": "{icon} {usage}%",
+      "format-icons": ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"]
+    },
+    "memory": {
+      "interval": 30,
+      "format": "{icon} {percentage}%",
+      "format-icons": ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"]
+    },
+    "clock": {
+      "interval": 1,
+      "format": " {:%H:%M:%S %a}",
+      "format-alt": " {:%Y-%m-%d}",
+      "tooltip-format": "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>", 
+    }
+  },
+  {
+    "layer": "top",
+    "position": "bottom",
+    "height": "22pt",
+    "spacing": 6,
+    "margin": 0,
+    "modules-left": ["network"],
+    "modules-center": [],
+    "modules-right": ["pulseaudio", "backlight", "tray"],
+    "network": {
+       "interface": "wlp1s0",
+       "interval": 2,
+      // "rotate": 30,
+      "format": "{icon} {essid} ({signalStrength}%)    {bandwidthUpOctets}  {bandwidthDownOctets}",
+      "format-alt":"{icon} {ipaddr}    {bandwidthUpOctets}  {bandwidthDownOctets}",
+      "format-icons": ["","", "", "", "", "", ""],
+      "tooltip-format": "{frequency}MHz - {ipaddr}\\{cidr}",
+      "format-disconnected": " Déconnecté",
+      "tooltip-format-disconnected": false,
+      "on-click-right": "kitty -e nmtui-connect"
+    },
+    "tray": {
+      "icon-size": 14,
+      "show-passive-items": true,
+      "spacing": 2
+    },
+    "pulseaudio": {
+      "scroll-step": 1,
+      "format": "{icon} {volume}%",
+      "format-bluetooth": " {volume}%",
+      "format-muted": "",
+      "format-icons": {
+        "headphones": "",
+        "handsfree": "",
+        "headset": "",
+        "phone": "",
+        "portable": "",
+        "car": "",
+        "default": ["","","","","",""]
+      },
+      "on-click": "pactl set-sink-mute @DEFAULT_SINK@ toggle",
+      "on-click-right": "pavucontrol"
+    },
+    "backlight": {
+      "format": "{icon}{percent}%",
+      "smooth-scrolling-threshold": 1,
+      "format-icons": ["", "", "", "", "", "", "", "", ""],
+      "on-scroll-up": "light -U 2 && light -O",
+      "on-scroll-down": "light -A 2 && light -O"
+    }
+  }
+]
