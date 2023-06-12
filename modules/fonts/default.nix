9797bf51c759abd49434d6558008b7669a9d7d19
zzzsyyy <me@zzzsy.top>
2023-06-12 14:20:27 +0800

    pkg: add custom Iosevka

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -6,12 +6,38 @@
       noto-fonts
       noto-fonts-cjk
       plangothic
-      my-fonts
+      #my-fonts
       noto-fonts-emoji
       (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
       (iosevka-bin.override { variant = "sgr-iosevka-term-ss05"; })
       # maple-mono
       wqy_zenhei
+      ((iosevka.override {
+        privateBuildPlan = {
+          family = "Iosevka ZT";
+          spacing = "term";
+          serifs = "sans";
+          no-cv-ss = false;
+          export-glyph-names = true;
+          variants = {
+            inherits = "ss05";
+            design = {
+              a = "single-storey-tailed";
+              i = "serifed-flat-tailed";
+              zero = "oval-tall-slashed";
+              three = "flat-top";
+              ampersand = "upper-open";
+              percent = "rings-continuous-slash-also-connected";
+            };
+          };
+          ligations.inherits = "dlig";
+        };
+        set = "zt";
+      }).overrideAttrs ( old: {
+          preBuild = ''
+            export NIX_BUILD_CORES=4;
+          '';
+        }))
     ];
     fontconfig = {
       localConf = builtins.readFile (./fontconfig.conf);
--- modules/home/programs/kitty/default.nix
+++ modules/home/programs/kitty/default.nix
@@ -3,5 +3,13 @@
 {
   programs.kitty = {
     enable = true;
+    font.name = "Iosevka ZT";
+    font.size = 12;
+    shellIntegration.enableFishIntegration = true;
+    settings = {
+      background_opacity = "0.95";
+      hide_window_decorations = true;
+    };
+    theme = "Nord";
   };
 }
