40c026e3834331bde5b4b4f72ec96a933f760a5c
zzzsyyy <me@zzzsy.top>
2023-07-15 20:46:17 +0800

    fix: megasync under hidpi

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -27,6 +27,14 @@
   };
   services.megasync = {
     enable = true;
+    package = pkgs.symlinkJoin {
+      name = "megasync";
+      paths = [ pkgs.megasync ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/megasync --prefix QT_SCALE_FACTOR : 1
+      '';
+    };
   };
   xdg.userDirs.enable = true;
   programs.home-manager.enable = true;
