b85c289e0f4e0a59b7b35ccd16484324b14031ed
zzzsyyy <me@zzzsy.top>
2024-02-25 13:34:25 +0800

    rime: fix flypy using patch

--- hosts/laptop/rime/home.nix
+++ hosts/laptop/rime/home.nix
@@ -3,7 +3,8 @@ let
   # dir = ".local/share/fcitx5/rime";
 in
 {
-  home.file."${dir}/default.custom.yaml".source = ./custom/default.custom.yaml;
-  home.file."${dir}/double_pinyin_flypy.custom.yaml".source = ./custom/double_pinyin_flypy.custom.yaml;
-  home.file."${dir}/rime_ice.custom.yaml".source = ./custom/rime_ice.custom.yaml;
+  home.file."${dir}" = {
+    source = ./custom;
+    recursive = true;
+  };
 }
--- pkgs/rime-ice/default.nix
+++ pkgs/rime-ice/default.nix
@@ -11,16 +11,15 @@ stdenvNoCC.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/share/rime-data
 
+    ${lib.optionalString enableUnihan ''
+      sed -e '9s/^ *# /  /' -i rime_ice.dict.yaml
+    ''}
+
     install -Dm644 *.{schema,dict}.yaml $out/share/rime-data
     install -Dm644 *.lua                 $out/share/rime-data
     install -Dm644 symbols*.yaml         $out/share/rime-data
     install -Dm644 default.yaml          $out/share/rime-data
-    
-    ${lib.optionalString enableUnihan ''
-      sed -e '9s/^ *# /  /' -i rime_ice.dict.yaml
-    ''}
-    sed -e '41s/^/# /' -e '43s/^ *# /    /' -i radical_pinyin.schema.yaml
-    
+
     mv cn_dicts en_dicts $out/share/rime-data
     mv opencc lua        $out/share/rime-data
   '';
